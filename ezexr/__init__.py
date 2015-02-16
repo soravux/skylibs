ff2e99c3d36a007cd8fb1cbb230b727ad4e59a59
Yannick Hold <yannickhold@gmail.com>
2015-02-16 12:54:19 +0000

    Began ezexr module

---
+++ ezexr/__init__.py
@@ -0,0 +1,42 @@
+import sys
+import array
+
+import OpenEXR
+import Imath
+
+import numpy as np
+
+
+def imread(filename):
+    """
+    Read an .exr image and returns a numpy matrix.
+    TODO:
+      - Support Alpha channel (and others)
+      - Support Greyscale
+    """
+    # Open the input file
+    f = OpenEXR.InputFile(filename)
+
+    # Read the three color channels as 32-bit floats
+    FLOAT = Imath.PixelType(Imath.PixelType.FLOAT)
+    (R,G,B) = [ array.array('f', f.channel(Chan, FLOAT)).tolist() for Chan in ("R", "G", "B") ]
+    return np.dstack((R, G, B))
+
+
+def imwrite(filename, arr):
+    """
+    Write an .exr file from an input <arr>ay.
+    TODO
+    """
+    h, w, d = arr.shape
+    # Convert to strings
+    (R, G, B) = [ array.array('f', Chan).tostring() for Chan in (arr[:,:,1], arr[:,:,2], arr[:,:,3]) ]
+
+    # Write the three color channels to the output file
+    out = OpenEXR.OutputFile(filename, OpenEXR.Header(h, w))
+    out.writePixels({'R' : R, 'G' : G, 'B' : G })
+
+
+imsave = imwrite
+
+__all__ = ['imread', 'imwrite', 'imsave']
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='skylibs',
+    version='0.1',
+    packages=['ezexr'],
+)
