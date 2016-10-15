18006feaf09a736b44584fa87adb4f3236390e70
Yannick Hold <yannickhold@gmail.com>
2016-10-15 11:28:49 -0400

    Added tools3d module

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='skylibs',
     version='0.1',
-    packages=['ezexr', 'envmap', 'hdrio', 'hdrtools', 'hdrtools/tonemapping', 'skydb'],
+    packages=['ezexr', 'envmap', 'hdrio', 'hdrtools', 'hdrtools/tonemapping', 'skydb', 'tools3d'],
     install_requires='openexr>=1.3.0',
     dependency_links=['https://github.com/jamesbowman/openexrpython/tarball/master#egg=openexr-1.3.0'],
 )
