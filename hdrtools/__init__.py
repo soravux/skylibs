aa804b77c4b9c5239b8788929b74bb022c502d51
Ian Maquignaz <ian.maquignaz.1@ulaval.ca>
2023-01-13 03:31:03 -0500

    Updated test cases

--- hdrtools/__init__.py
+++ hdrtools/__init__.py
@@ -1,2 +1,2 @@
 from .gsolve import gsolve, weights
-from .sunutils import findBrightestSpot, sunPosFromEnvmap, sunPositionFromPySolar
+from .sunutils import findBrightestSpot, sunPosition_fromEnvmap, sunPosition_pySolar_zenithAzimuth, sunPosition_pySolar_XYZ
